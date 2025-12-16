853146797cf15f877ab9138a893f5ab4bcb42395
Bhavya2007-18 <bhavyaporwal007@gmail.com>
2025-12-16 21:20:57 +0530

    new

--- c-loops.c
+++ c-loops.c
@@ -1,3 +1,16 @@
 // ### for loop
 
 // **entry-controlled** loop, which means that the condition is checked before the loop's body executes
+#include <stdio.h>
+ int main(){
+
+    // for loop syntax
+    // for ( starting value; condition; increment/decrement ) {
+    //              work to be done; }
+
+    for(int i =0; i<=10; i++){
+        printf("hum kyun kr rahe hai c bc??\n");
+    }
+ }
+
+ 
