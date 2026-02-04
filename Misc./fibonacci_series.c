c6627562676e66b1e2138311407d5ff8642cd127
Prakhar1808 <prakharsharma.home@gmail.com>
2026-02-04 18:36:12 +0530

    docs(fibonacci): add comments regarding inefficiency
    + the code was already fairly explained, added the TODO tag for future work to be done

--- Misc./fibonacci_series.c
+++ Misc./fibonacci_series.c
@@ -1,4 +1,5 @@
 // fibonacci_recursive_basic.c
+// TODO:a more efficient and simple version
 #include <stdio.h>
 
 // Function declaration
@@ -6,27 +7,27 @@ int fibonacci(int n);
 
 int main() {
     int n;
-    
+
     printf("Enter the number of terms: ");
     scanf("%d", &n);
-    
+
     printf("Fibonacci Series (first %d terms):\n", n);
     for(int i = 0; i < n; i++) {
         printf("%d ", fibonacci(i));
     }
     printf("\n");
-    
+
     // Get specific term
     printf("\nEnter term position to find (0-indexed): ");
     int position;
     scanf("%d", &position);
     printf("Fibonacci(%d) = %d\n", position, fibonacci(position));
-    
+
     return 0;
 }
 
 // Recursive function to find nth Fibonacci number
-// Base cases: 
+// Base cases:
 //   fibonacci(0) = 0
 //   fibonacci(1) = 1
 // Recursive case:
@@ -34,5 +35,6 @@ int main() {
 int fibonacci(int n) {
     if(n == 0) return 0;
     if(n == 1) return 1;
-    return fibonacci(n - 1) + fibonacci(n - 2);
+    return fibonacci(n - 1) + fibonacci(n - 2); //the part which makes it Recursive, inefficient but works
+    //mulitple times a single value is calculated which makes it so much inefficient
 }
